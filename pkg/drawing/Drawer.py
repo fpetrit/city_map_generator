f38e2b915c83e3d5a222992149a1c68234f42dbb
fpetrit <fre.petrit@stanislas-cannes.com>
2022-04-27 21:00:00 +0200

    supprimer le systeme de dessin

--- pkg/drawing/Drawer.py
+++ pkg/drawing/Drawer.py
@@ -1,26 +1,25 @@
 from typing import Tuple
 from tkinter import Canvas
 import random as ra, turtle as tu
-from queue import Queue
-from threading import Thread
 
-from pkg.drawing.drawable.House import House
 from pkg.config.config import *
+
+
+
 
 class Drawer:
     """
     Draw a randomly generated 2D town plan when start() is called (blocking function), on the given tkinter canvas and parameters passed when instantiating.
-    "turtle" module is used to draw. Working principle : a queue.Queue is fill with drawnig instructions in a thread, dequeued and executed at the same time in
-    the main thread (turtle and tkinter calls must be in the main thread).
+    "turtle" module is used to draw. Working principle : 
     """
 
     def __init__(self, canvas : Canvas, parameters : dict) -> None :
         self.parameters = parameters
-        self.__ts = tu.TurtleScreen(canvas)
+        self._ts = tu.TurtleScreen(canvas)
         # Unleash the turtle's speed : set the 'actions per refresh' number, and the delay
-        self.__ts.tracer(ACTIONS_PER_SCREEN_REFRESH, DELAY)
-        self.__turtle = tu.RawTurtle(self.__ts)
-        self.__turtle.speed(0)
+        self._ts.tracer(ACTIONS_PER_SCREEN_REFRESH, DELAY)
+        self._turtle = tu.RawTurtle(self._ts)
+        self._turtle.speed(0)
 
 
 
@@ -34,56 +33,5 @@ class Drawer:
     def start(self):
         """Starts generating and drawing the map on the canvas. Blocking function."""
 
-        # Create an infinite instructions queue
-        self.__drawing_instructions = Queue(0)
-
-        # Start the scheduling in a separate thread.
-        schedule_thread = Thread(target = self.schedule)
-        schedule_thread.start()
-        # Start executing the instructions.
-        self.draw()
-
-        # Wait for the thread to end
-        schedule_thread.join()
-
-
-
-    def schedule(self) -> None :
-        """Starts creating and queueing instructions. Must be called in a thread."""
-
-        queue = self.__drawing_instructions
-
-        # The lines below are just FOR TESTING
-
-        for _ in range(20) :
-            queue.put("house")
-
-        queue.put(False)
-
-
-
-    def draw(self) -> None :
-        """Starts executing the drawing instructions in the queue. Blocks until it has dequeued an ending flag."""
-
-        queue = self.__drawing_instructions
-        tu = self.__turtle
-        params = self.parameters
-        instruction = -1
-        coordinates_system = [0, int(params["width"]), 0, int(params["height"])]
-
-        self.__ts.setworldcoordinates(0, 0, int(params["width"]), int(params["height"]))
-
-        while instruction != False :
-            # Get an instruction and do some work on the canvas.
-
-            instruction = queue.get()
-
-            # Switch with if, elif, ..., else
-
-            # Determine a width, a heigth
-            if instruction == "house" :
-
-                
-                place = [(self.get_random_tuple(*coordinates_system), self.get_random_tuple(*coordinates_system))]
-                House(places = place).draw(tu)
+        pass
 
